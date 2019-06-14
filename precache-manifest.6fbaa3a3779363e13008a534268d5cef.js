842f72095ffa526a0f537aea6e08f11c31046b39
xly <328704719@qq.com>
2019-06-14 15:08:08 +0800

    Updates

---
+++ precache-manifest.6fbaa3a3779363e13008a534268d5cef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e2771fbb49c9828a4e15fd03e25832",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "93d6fd7d95aa83f2a380",
+    "url": "/myapp/static/css/2.4aa378bc.chunk.css"
+  },
+  {
+    "revision": "f8469adcf0be461c549b",
+    "url": "/myapp/static/css/main.66b6aded.chunk.css"
+  },
+  {
+    "revision": "93d6fd7d95aa83f2a380",
+    "url": "/myapp/static/js/2.7c9f4262.chunk.js"
+  },
+  {
+    "revision": "f8469adcf0be461c549b",
+    "url": "/myapp/static/js/main.af009b21.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "8820b7f6582a3c45b7527ae6b183dd2f",
+    "url": "/myapp/static/media/mui.8820b7f6.ttf"
+  }
+]);
